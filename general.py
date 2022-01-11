0902276d6d59fb34714f3091b91eba34ddbecb69
kuanshi <kuanshi@hotmail.com>
2022-01-10 22:56:09 -0800

    kz - packaging the app (still under construction)

---
+++ general.py
@@ -0,0 +1,224 @@
+# Constants, variables, and methods that are commonly used
+
+import os
+from datetime import datetime
+import numpy as np
+import pandas as pd
+
+class Logfile:
+    def __init__(self, logfile_dir = './', logfile_name = 'saf_ida.log', screen_msg = True):
+        """
+        Initializing the logfile
+        - logfile_dir: default is the same path of the PLoM package
+        - logfile_name: default is the "saf_ida.log"
+        - screen_msg: default is to show message on screen
+        """
+        self.logfile_dir = logfile_dir
+        self.logfile_name = logfile_name
+        self.logfile_path = os.path.join(self.logfile_dir, self.logfile_name)
+        self.screen_msg = screen_msg
+        # start the log
+        self.write_msg(msg = '--NEW LOG STARTING FROM THIS LINE--', mode='w')            
+    
+    def write_msg(self, msg = '', msg_type = 'RUNNING', msg_level = 0, mode='a'):
+        """
+        Writing running messages
+        - msg: the message
+        - msg_type: the type of message 'RUNNING', 'WARNING', 'ERROR'
+        - msg_level: how many indent tags
+        """
+        indent_tabs = ''.join(['\t']*msg_level)
+        decorated_msg = '{} {} {}-MSG {} '.format(datetime.utcnow(), indent_tabs, msg_type, msg)
+        if self.screen_msg:
+            print(decorated_msg)
+        with open(self.logfile_path, mode) as f:
+            f.write('\n'+decorated_msg)
+    
+    def delete_logfile(self):
+        """
+        Deleting the log file
+        """
+        if os.path.exists(self.logfile_path):
+            os.remove(self.logfile_path)
+        else:
+            print('The logfile {} does not exist.'.format(self.logfile_path))
+
+
+class DBServer:
+    def __init__(self, db_dir = './', db_name = 'saf_ida.h5'):
+        """
+        Initializing the database
+        - db_dir: default is the same path of the PLoM package
+        - db_name: default is "saf_ida.h5"
+        """
+        self.db_dir = db_dir
+        self.db_name = db_name
+        self.db_path = os.path.join(self.db_dir, self.db_name)
+        if os.path.exists(self.db_path):
+            # deleting the old database
+            os.remove(self.db_path)
+        self.init_time = datetime.utcnow()
+        self.item_name_list = []
+        self.basic()
+        self.dir_export = self._create_export_dir()
+        self._item_list = ITEM_LIST
+        self._item_adds = ITEM_ADDS
+            
+    
+    def basic(self):
+        """
+        Writing basic info
+        """
+        df = pd.DataFrame.from_dict({
+            'InitializedTime': [self.init_time],
+            'LastEditedTime': [datetime.utcnow()],
+            'DBName': [self.db_name],
+        }, dtype=str)
+        store = pd.HDFStore(self.db_path, 'a')
+        df.to_hdf(store, 'basic', mode='a')
+        store.close()
+
+
+    def _create_export_dir(self):
+        """
+        Creating a export folder
+        """
+        dir_export = os.path.join(self.db_dir,'DataOut')
+        try:
+            os.makedirs(dir_export, exist_ok=True)
+            return dir_export
+        except:
+            return None
+
+
+    def get_item_adds(self):
+        """
+        Returning the full list of data items
+        """
+        return self._item_adds
+
+
+    def add_item(self, item_name = None, col_names = None, item = [], data_shape = None, data_type='Data'):
+        """
+        Adding a new data item into database
+        """
+        if data_type == 'Data':
+            if item.size > 1:
+                df = pd.DataFrame(item, columns = col_names)
+                dshape = pd.DataFrame(data_shape, columns=['DS_'+item_name])
+            else:
+                if col_names is None:
+                    col_names = item_name
+                df = pd.DataFrame.from_dict({
+                    col_names: item.tolist()
+                })
+                dshape = pd.DataFrame.from_dict({
+                    'DS_'+col_names: (1,)
+                })
+            if item_name is not None:
+                store = pd.HDFStore(self.db_path, 'a')
+                # data item
+                df.to_hdf(store, item_name, mode='a')
+                # data shape
+                dshape.to_hdf(store, 'DS_'+item_name, mode='a')
+                store.close()
+        elif data_type == 'ConstraintsFile':
+            # constraints filename
+            cf = pd.DataFrame.from_dict({
+                'ConstraintsFile': item
+            }, dtype=str)
+            store = pd.HDFStore(self.db_path, 'a')
+            cf.to_hdf(store, 'constraints_file', mode='a')
+            store.close()
+        else:
+            # Not supported data_type
+            return False
+
+
+    def get_item(self, item_name = None, table_like=False, data_type='Data'):
+        """
+        Getting a specific data item
+        """
+        if data_type == 'Data':
+            if item_name is not None:
+                store = pd.HDFStore(self.db_path, 'r')
+                try:
+                    item = store.get(item_name)
+                    item_shape = tuple([x[0] for x in self.get_item_shape(item_name=item_name).values.tolist()])
+                    if not table_like:
+                        item = item.to_numpy().reshape(item_shape)                
+                except:
+                    item = None
+                finally:
+                    store.close()
+
+                return item
+
+            return item.values.tolist()[0][0]
+
+
+    def remove_item(self, item_name = None):
+        """
+        Removing an item
+        """
+        if item_name is not None:
+            store = pd.HDFStore(self.db_path, 'r')
+            try:
+                store.remove(item_name)
+            except:
+                item = None
+            finally:
+                store.close()
+
+
+    def get_item_shape(self, item_name = None):
+        """
+        Getting the shape of a specific data item
+        """
+        
+        if item_name is not None:
+            store = pd.HDFStore(self.db_path, 'r')
+            try:
+                item_shape = store.get('DS_'+item_name)
+            except:
+                item_shape = None
+            store.close()
+
+            return item_shape
+
+
+    def get_name_list(self):
+        """
+        Returning the keys of the database
+        """
+        store = pd.HDFStore(self.db_path, 'r')
+        try:
+            name_list = store.keys()
+        except:
+            name_list = []
+        store.close()
+        return name_list
+
+
+    def export(self, data_name = None, filename = None, file_format = 'csv'):
+        """
+        Exporting the specific data item
+        - data_name: data tag
+        - format: data format
+        """
+        d = self.get_item(item_name = data_name[1:], table_like=True)
+        if d is None:
+            return 1
+        if filename is None:
+            filename = os.path.join(self.dir_export,str(data_name).replace('/','')+'.'+file_format)
+        else:
+            filename = os.path.join(self.dir_export,filename.split('.')[0]+'.'+file_format)
+        if file_format == 'csv' or 'txt':
+            d.to_csv(filename, header=True, index=True)
+        elif file_format == 'json':
+            with open(filename, 'w') as f:
+                json.dump(d, f)
+        else:
+            return 2
+        return filename
+        
---
+++ saf-ida.py
@@ -0,0 +1,246 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2021 Leland Stanford Junior University
+# Copyright (c) 2021 The Regents of the University of California
+#
+# This file is part of the SimCenter Backend Applications
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# You should have received a copy of the BSD 3-Clause License along with
+# this file. If not, see <http://www.opensource.org/licenses/>.
+#
+# Contributors:
+# Kuanshi Zhong
+#
+
+import argparse, json, os
+from general import *
+from pyngms import NestedGroundMotionSelection as NGMS
+
+class SAF_IDA:
+
+    def __init__(self, dir_info = dict(), job_name = 'saf_ida'):
+
+        # initiate a log file
+        self.output_dir = dir_info.get('Output', './')
+        self.logfile_name = job_name+'.log'
+        self.logfile = Logfile(logfile_dir=self.output_dir, logfile_name=self.logfile_name)
+
+        # initiate a database
+        self.db_name = job_name+'.h5'
+        self.dbserver = DBServer(db_dir=self.output_dir, db_name=self.db_name)
+
+        # initiate a nested ground motion set
+        self.groundmotions = NGMS.NestedGroundMotionSet(job_name=job_name)
+
+    def _parse_gms_config(self, gms_config):
+
+        # initialize intensity measure grid
+        self.groundmotions.definekeyim(0, [])
+
+        # intensity measure grid
+        im_grid_config = gms_config.get('IntensityMeasureGrid', None)
+        if im_grid_config is None:
+            err_msg = 'SAF_IDA._parse_gms_config: IntensityMeasureGrid not found in configuration.'
+            self.logfile.write_msg(msg=err_msg, msg_type='ERROR')
+            return 1
+        # im grid dimension
+        im_grid_dim = len(im_grid_config)
+        if im_grid_dim == 0:
+            err_msg = 'SAF_IDA._parse_gms_config: no intensity measure defined in IntensityMeasureGrid.'
+            self.logfile.write_msg(msg=err_msg, msg_type='ERROR')
+            return 1
+        # im names
+        im_names = list(im_grid_config.keys())
+        for cur_im in im_names:
+            im_info = im_grid_config.get(cur_im, None)
+            if im_info is None:
+                err_msg = 'SAF_IDA._parse_gms_config: empty in {}.'.format(cur_im)
+                self.logfile.write_msg(msg=err_msg, msg_type='ERROR')
+                return 1
+            # im grid mesh
+            cur_mesh = im_info.get('Mesh', None)
+            if cur_mesh is None:
+                err_msg = 'SAF_IDA._parse_gms_config: IntensityMeasureGrid missing Mesh for {}.'.format(cur_im)
+                self.logfile.write_msg(msg=err_msg, msg_type='ERROR')
+                return 1
+            # get range
+            cur_range = im_info.get('Range', None)
+            if cur_range is None:
+                err_msg = 'SAF_IDA._parse_gms_config: IntensityMeasureGrid missing Range for {}.'.format(cur_im)
+                self.logfile.write_msg(msg=err_msg, msg_type='ERROR')
+                return 1
+            # T1, Ta, and Tb
+            if 'SaRatio' in cur_im:
+                T1 = im_info.get('T1', None)
+                Ta = im_info.get('Ta', None)
+                Tb = im_info.get('Tb', None)
+                if None in [T1, Ta, Tb]:
+                    err_msg = 'SAF_IDA._parse_gms_config: SaRatio missing T1, Ta, Tb'
+                    self.logfile.write_msg(msg=err_msg, msg_type='ERROR')
+                    return 1
+                # add im
+                self.groundmotions.add_key_im(name=cur_im, vrange=cur_range, mesh=cur_mesh, T1=T1, Ta=Ta, Tb=Tb)
+            else:
+                # add im
+                self.groundmotions.add_key_im(name=cur_im, vrange=cur_range, mesh=cur_mesh)
+        
+        # scaling limit if any
+        scaling_config = gms_config.get('Scaling', None)
+        if scaling_config is not None:
+            # minimum scaling
+            sf_min = scaling_config.get('Min', 0.001)
+            sf_max = scaling_config.get('Max', 1.0e6)
+            sf_penalty = scaling_config.get('Penalty', 0.0)
+            sf_t = scaling_config.get('ReferenceT', None)
+            sf_sa = scaling_config.get('ReferenceSa', None)
+            if None in [sf_t, sf_sa]:
+                err_msg = 'SAF_IDA._parse_gms_config: Scaling missing ReferenceT, ReferenceSa'
+                self.logfile.write_msg(msg=err_msg, msg_type='ERROR')
+                return 1
+            self.groundmotions.scalinglimit(sf_min, sf_max, sf_penalty, sf_t, sf_sa)            
+
+        # ground motion database
+        self.gmdb_file = gms_config.get('GroundMotionData', 
+            os.path.join(os.path.dirname(os.path.abspath(__file__)), 'pyngms', 'data', 'GroundMotionCharacteristics.csv'))
+
+        # return
+        return 0
+
+
+    def _select_records(self):
+
+        try:
+            # generate grid
+            self.groundmotions.generategrid()
+        except:
+            err_msg = 'SAF_IDA._select_records: failed to generate IM grid'
+            self.logfile.write_msg(msg=err_msg, msg_type='ERROR')
+            return 1
+        run_msg = 'SAF_IDA._select_records: IM grid generated'
+        self.logfile.write_msg(msg=run_msg)
+
+        try:
+            # select records        
+            self.groundmotions.selectnestedrecord(gmdb_path=self.gmdb_file)
+        except:
+            err_msg = 'SAF_IDA._select_records: failed to select ground motions'
+            self.logfile.write_msg(msg=err_msg, msg_type='ERROR')
+            return 1
+        run_msg = 'SAF_IDA._select_records: ground motions selected'
+        self.logfile.write_msg(msg=run_msg)
+
+        try:
+            # save data
+            self.groundmotions.savedata(output_path=self.output_dir)
+        except:
+            err_msg = 'SAF_IDA._select_records: failed to save data'
+            self.logfile.write_msg(msg=err_msg, msg_type='ERROR')
+            return 1
+        run_msg = 'SAF_IDA._select_records: data saved'
+        self.logfile.write_msg(msg=run_msg)
+
+        return 0
+
+
+    def create_groundmotionset(self, gms_config = None):
+        """
+        create a nested ground motion set
+        input:
+            gms_config: a dictionary of ground motion selection configuration
+        """
+
+        # load gms configuration
+        if self._parse_gms_config(gms_config):
+            err_msg = 'SAF_IDA.config_groundmotions: error in parsing ground motion selection configurations.'
+            self.logfile.write_msg(msg=err_msg, msg_type='ERROR')
+            return 1
+        self.logfile.write_msg(msg='SAF_IDA.config_groundmotions: ground motion selection configured.')
+
+        # select records
+        if self._select_records():
+            err_msg = 'SAF_IDA.config_groundmotions: error in selecting ground motions.'
+            self.logfile.write_msg(msg=err_msg, msg_type='ERROR')
+            return 1
+        self.logfile.write_msg(msg='SAF_IDA.config_groundmotions: ground motion selection completed.')
+
+
+def run_saf_ida(job_name = 'saf_ida', job_config = ''):
+
+    # read job configuration
+    if not os.path.exists(args.job_config):
+        print('run_saf_ida: configuration file not found: {}'.format(args.job_config))
+        return 1
+    try:
+        with open(args.job_config) as f:
+            job_info = json.load(f)
+    except:
+        print('run_saf_ida: malformatted configuration file: {}'.format(args.job_config))
+        return 1
+
+    # directory
+    dir_info = job_info['Directory']
+    work_dir = dir_info['Work']
+    input_dir = dir_info['Input']
+    output_dir = dir_info.get('Output',None)
+    if output_dir is None:
+        output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__),'Output'))
+    try:
+        os.mkdir(f"{output_dir}")
+    except:
+        print('runSAF_IDA: output directory already exists.')
+
+    # job type
+    job_type = job_info.get('Type',['GroundMotionSelection','HazardConsistentAdjustment'])
+
+    # create SAF_IDA job
+    saf_ida_job = SAF_IDA(dir_info=dir_info, job_name=job_name)
+
+    # run jobs
+    if 'GroundMotionSelection' in job_type:
+        # get the ground motion selection config.
+        gms_config = job_info.get('GroundMotionSelection', None)
+        if gms_config is None:
+            err_msg = 'run_saf_ida: GroundMotionSelection not found in job configuration.'
+            saf_ida_job.logfile.write_msg(msg=err_msg, msg_type='ERROR')
+            return 1
+
+        # create ground motions
+        saf_ida_job.create_groundmotionset(gms_config=gms_config)
+
+
+if __name__ == '__main__':
+
+    # parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--job_name')
+    parser.add_argument('--job_config')
+    args = parser.parse_args()
+
+    # run saf-ida
+    run_saf_ida(job_name = args.job_name, job_config = args.job_config)
